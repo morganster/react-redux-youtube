ac68f96d9f9b0521bad248d16f7a5773180c2146
Carlos Jaramillo <granmorgan@gmail.com>
2018-03-22 07:35:41 -0500

    remove keys

--- src/Services/YoutubeService.js
+++ src/Services/YoutubeService.js
@@ -3,13 +3,12 @@ import axios from 'axios';
 export class YouTubeService {
     // configuration
     url = 'https://www.googleapis.com/youtube/v3/search?part=id,snippet&q=';
-    token = 'AIzaSyDaKQ5Wk5C4KLF81e8L2uD6kjOamlFYxn0';
     
     getVideosFromApi(searchTerm, related) {
       var options={
               // channelId : 'UC2Lg_JM84ehrW4bs85SWtLw',
               maxResults :20,
-              key: 'AIzaSyDaKQ5Wk5C4KLF81e8L2uD6kjOamlFYxn0',
+              key: '',
               type:'video'
           }
       if(related){
